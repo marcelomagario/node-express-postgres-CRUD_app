467b0e2515911c21078281bb66270d9da040cc52
Marcelo <marcelomagario@gmail.com>
2023-04-18 16:44:58 -0300

    Command GET and server sending "Hello" message

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 const port = 3000;
+
+app.get('/', (request, response)=>{
+    response.send('hello');        
+});
 app.listen(port, ()=>{
-   console.log(`server started on ${port}`);
+    console.log(`server started on ${port}`);
 });
