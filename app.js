9750637bcff34b0b079af25fa4a7bf12d544d845
Marcelo <marcelomagario@gmail.com>
2023-04-18 22:25:41 -0300

    Deleted app.use and its working now

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 
 const port = 3000;
 
-app.use((request, response, next)=>{
-    console.log('ran in between server call-updated')
-})
+// app.use((req, res, next)=>{
+//     console.log('ran in between server call');
+// });
 
-app.get('/', (request, response)=>{
-    response.send('HELLO');        
+app.get('/', (req, res)=>{
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="/info/get" method ="GET">
+        <input type="submit" value="GET">
+        </form>
+    </body>
+    </html>`);        
 });
 app.listen(port, ()=>{
     console.log(`server started on ${port}`);
