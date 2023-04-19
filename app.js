75ed54dc5b8cf983028997b67302e1e6399455ae
Marcelo <marcelomagario@gmail.com>
2023-04-18 21:20:50 -0300

    Insert GET to return a value

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const { response } = require('express');
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
+app.use((request, response, next)=>{
+    console.log('ran in between server call-updated')
+})
+
 app.get('/', (request, response)=>{
-    response.send('hello');        
+    response.send('HELLO');        
 });
 app.listen(port, ()=>{
     console.log(`server started on ${port}`);
